be923fbe0217ef03f4f4162c9ec6c0f1a48355c2
Farhan-Khan7 <fk5518744@gmail.com>
2025-08-25 11:10:14 +0530

    First Task Completer

---
+++ Task1.js
@@ -0,0 +1,12 @@
+// Step 1: Object banaya
+let student = {
+    name: "Farhan",
+    age: 18,
+    grade: "A"
+};
+
+// Step 2: Print details
+console.log("Student Details:");
+console.log("Name: " + student.name);
+console.log("Age: " + student.age);
+console.log("Grade: " + student.grade);
